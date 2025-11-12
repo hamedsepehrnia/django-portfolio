9cfe9ba2691e46fa3ec3e02373be06b856f4c7a2
hamed <hamed.sepehrnia85@gmail.com>
2025-11-12 17:39:03 +0330

    fixed photo

--- skypardaz/urls.py
+++ skypardaz/urls.py
@@ -31,6 +31,7 @@ urlpatterns += i18n_patterns(
     prefix_default_language=True,
 )
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
